b27a086a7085c30efc1a0c8054c41d42d6094d0d
guxukai <gxk@33.cn>
2023-01-16 20:29:21 +0800

    chore:add client-v2;

---
+++ client-v2/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<RouterView />
+</template>
+
+<style>
+html,
+body {
+	margin: 0;
+}
+
+main {
+	margin: calc((100vh / 25) * 1.563) calc((100vw / 25) * 1.563);
+}
+</style>
---
+++ client-v2/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+function randomString() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+function start() {
+  router.push({
+    name: 'editor',
+    params: {
+      id: randomString()
+    },
+  })
+}
+</script>
+
+<template>
+  <main>
+    <h1>Dragons can code</h1>
+    <p>Let's start collaborating on some code.</p>
+    <button @click="start">Start coding</button>
+  </main>
+</template>
